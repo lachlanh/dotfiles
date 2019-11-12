3b768269bf3869425c1b1fd280df548ac980f62f
lachlanh <lhespe@opentable.com>
2019-11-12 21:58:37 +0100

    reorg emacs config, [wip]

--- to_git.sh
+++ to_git.sh
@@ -4,9 +4,13 @@ cp ~/.zshrc ./.zshrc
 cp ~/.bash_profile ./.bash_profile
 cp ~/.gitconfig ./.gitconfig
 cp ~/.emacs.d/init.el ./.emacs.d/init.el
+cp ~/.emacs.d/ui.el ./.emacs.d/ui.el
+cp ~/.emacs.d/packages.el ./.emacs.d/packages.el
+cp ~/.emacs.d/navigation.el ./.emacs.d/navigation.el
+cp ~/.emacs.d/editing.el ./.emacs.d/editing.el
+cp ~/.emacs.d/programming.el ./.emacs.d/programming.el
 cp ~/.profile ./.profile
 cp ~/.compton.conf ./.compton.conf
 #use -anv for dry run
-rsync -a ~/.emacs.d/customizations/ ./.emacs.d/customizations
 rsync -a ~/.config/i3/ ./.config/i3
 rsync -a ~/.config/alacritty/ ./.config/alacritty
