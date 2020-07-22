c99ea19259d415d6598117b7848d6b37940fe8b7
lachlanh <lachlan@hesp.es>
2020-07-22 14:02:52 +0200

    update fish config using fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,10 @@
 set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
 
 set -x WORK ~/src
+
+set -Ux EDITOR emacsclient -t
 # update the path
 set PATH /home/lach/.npm-global/bin $PATH
-# THEME PURE #
-set fish_function_path /home/lach/.config/fish/functions/theme-pure/functions/ $fish_function_path
-source /home/lach/.config/fish/functions/theme-pure/conf.d/pure.fish
+
+
+
