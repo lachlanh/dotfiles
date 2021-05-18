dbb017eac123f02bd5fb3a32fd9d556cd49790a7
lachlanh <lachlan@hesp.es>
2021-05-18 21:07:28 +0200

    xset rate and remove go path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,16 +2,22 @@
 set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
 
 set -x WORK ~/src/wfm
-set -x GOPATH $WORK/go
+
 set -x FZFPATH ~/.fzf/bin
 
 
 set -Ux EDITOR emacsclient -t
 # update the path
-set PATH /usr/local/go/bin $PATH
-set PATH $GOPATH/bin $PATH
+# go bits
+# set -x GOPATH $WORK/go
+# set PATH /usr/local/go/bin $PATH
+# set PATH $GOPATH/bin $PATH
+
+# set PATH /home/lach/.npm-global/bin $PATH
+
 set PATH $FZFPATH $PATH
-set PATH /home/lach/.npm-global/bin $PATH
 
+set PATH $HOME/bin $PATH
 
-
+# work
+source $WORK/wefarm.fish
