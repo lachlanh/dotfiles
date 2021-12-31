74f774e53ad4880f50ec2334db4de604a9654702
lachlanh <lachlan@hesp.es>
2021-12-31 16:09:15 +0100

    remove bash profile add qutebrowser

--- from_git.sh
+++ from_git.sh
@@ -2,7 +2,7 @@
 cp .tmux.conf ~/.tmux.conf
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
-cp .bash_profile ~/.bash_profile
+#cp .bash_profile ~/.bash_profile
 cp .gitconfig ~/.gitconfig
 cp .profile ~/.profile
 cp .xsession ~/.xsession
@@ -13,7 +13,8 @@ cp .compton.conf ~/.compton.conf
 #rsync -a ./.vim/autoload/ ~/.vim/autoload
 #rsync -a ./.emacs.d/ ~/.emacs.d
 rsync -a ./.config/i3/ ~/.config/i3
-rsync -a ./.config/alacritty/ ~/.config/alacritty
-rsync -a ./.config/sway/ ~/.config/sway
+#rsync -a ./.config/alacritty/ ~/.config/alacritty
+sync -a ./.config/qutebrowser/ ~/.config/qutebrowser
+sync -a ./.config/sway/ ~/.config/sway
 rsync -a ./.config/fish/ ~/.config/fish
 rsync -a ./.config/systemd/ ~/.config/systemd
