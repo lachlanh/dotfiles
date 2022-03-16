41d624eb90860a8b772b33ebcda2e7cf13945a83
lachlanh <lachlan@hesp.es>
2022-03-16 20:00:56 +0100

    fish_add_path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,10 +8,12 @@ set -x FZFPATH ~/.fzf/bin
 
 set -Ux EDITOR emacsclient -t
 # update the path
-set PATH /usr/local/go/bin $PATH
-set PATH $GOPATH/bin $PATH
-set PATH $FZFPATH $PATH
+# set PATH /usr/local/go/bin $PATH
+# set PATH $GOPATH/bin $PATH
+# set PATH $FZFPATH $PATH
 # set PATH /home/lach/.npm-global/bin $PATH
+
+fish_add_path $HOME/bin
 
 source $WORK/wefarm.fish
 
