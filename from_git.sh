c15a5d032fe5f1729c313a0cb44dcea4e98f5442
lachlanh <lachlan@hesp.es>
2022-05-04 10:21:59 +0200

    orbiter changes to i3 and i3status

--- from_git.sh
+++ from_git.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 cp .tmux.conf ~/.tmux.conf
-cp .vimrc ~/.vimrc
-cp .zshrc ~/.zshrc
-cp .bash_profile ~/.bash_profile
+#cp .vimrc ~/.vimrc
+#cp .zshrc ~/.zshrc
+#cp .bash_profile ~/.bash_profile
 cp .gitconfig ~/.gitconfig
 cp .profile ~/.profile
 cp .xsession ~/.xsession
 cp .Xresources ~/.Xresources
-cp .compton.conf ~/.compton.conf
+#cp .compton.conf ~/.compton.conf
 #use -anv for dry run
 #rsync -a ./.vim/rc/ ~/.vim/rc
 #rsync -a ./.vim/autoload/ ~/.vim/autoload
 #rsync -a ./.emacs.d/ ~/.emacs.d
 rsync -a ./.config/i3/ ~/.config/i3
-rsync -a ./.config/alacritty/ ~/.config/alacritty
+rsync -a ./.config/i3status/ ~/.config/i3status
+#rsync -a ./.config/alacritty/ ~/.config/alacritty
 rsync -a ./.config/sway/ ~/.config/sway
 rsync -a ./.config/fish/ ~/.config/fish
 rsync -a ./.config/systemd/ ~/.config/systemd
--- to_git.sh
+++ to_git.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 cp ~/.tmux.conf ./.tmux.conf
-cp ~/.vimrc ./.vimrc
-cp ~/.zshrc ./.zshrc
-cp ~/.bash_profile ./.bash_profile
+#cp ~/.vimrc ./.vimrc
+#cp ~/.zshrc ./.zshrc
+#cp ~/.bash_profile ./.bash_profile
 cp ~/.gitconfig ./.gitconfig
 #cp ~/.emacs.d/init.el ./.emacs.d/init.el
 cp ~/.profile ./.profile
 cp ~/.xsession ./.xsession
 cp ~/.Xresources ./.Xresources
-cp ~/.compton.conf ./.compton.conf
+#cp ~/.compton.conf ./.compton.conf
 #use -anv for dry run
 #rsync -a ~/.emacs.d/config/ ./.emacs.d/config
 #rsync -a ~/.emacs.d/themes/ ./.emacs.d/themes
 rsync -a ~/.config/i3/ ./.config/i3
-rsync -a ~/.config/alacritty/ ./.config/alacritty
+rsync -a ~/.config/i3status/ ./.config/i3status
+#rsync -a ~/.config/alacritty/ ./.config/alacritty
 rsync -a ~/.config/sway/ ./.config/sway
 rsync -a ~/.config/fish/ ./.config/fish
 rsync -a ~/.config/systemd/ ./.config/systemd
