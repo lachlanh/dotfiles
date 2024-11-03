d0498a3092e0020828018b8fcdd95931d411b498
lachlanh <lachlan@hesp.es>
2024-11-03 09:34:58 +0100

    Xterm settings, fish setup, i3status

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,12 @@
 # use rg in fzf
 set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
 
-set -x WORK ~/src/doc
+set -x WORK ~/src/ocean
 
 set -x FZFPATH ~/.fzf/bin
 
-
 set -x EDITOR emacsclient -t
+
 # update the path
 # go bits
 # set -x GOPATH $WORK/go
@@ -15,12 +15,24 @@ set -x EDITOR emacsclient -t
 
 # set PATH /home/lach/.npm-global/bin $PATH
 
-set PATH $FZFPATH $PATH
+fish_add_path -g -p $FZFPATH
+fish_add_path -g -p $HOME/bin
+fish_add_path -g -p $HOME/bin/flutter/bin
 
-set PATH $HOME/bin $PATH
+# android bits
+# set -x ANDROID_HOME ~/Android/Sdk
+
+# fish_add_path -g -p $ANDROID_HOME/cmdline-tools/latest/bin
+# fish_add_path -g -p $ANDROID_HOME/platform-tools
+
+#flutterfire
+fish_add_path -g -p $HOME/.pub-cache/bin
+
+#set PATH $FZFPATH $PATH
+#set PATH $HOME/bin $PATH
 
 # work
-#source $WORK/wefarm.fish
+source $WORK/ob.fish
 
 # random sway bits
-set -x MOZ_ENABLE_WAYLAND 1
+#set -x MOZ_ENABLE_WAYLAND 1
